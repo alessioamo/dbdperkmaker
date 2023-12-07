fb0e085cf7be2c557ca95af00b9800f622998397
Alessio <alessio.amorelli@hotmail.com>
2023-12-07 03:39:29 -0500

    Saved images have transparent background
    - Saved images from all tabs (including icon creator) are now transparent

--- saveImage.js
+++ saveImage.js
@@ -9,24 +9,25 @@ btns.forEach((btn) => {
             if (document.getElementById(divId[i]).style.display == "block") {
                 if (divId[i] == "iconEditor") {
                     const srcElement = document.querySelector("#iconCanvas");
-                    html2canvas(srcElement).then((canvas) => {
+                    srcElement.style.backgroundColor = "transparent";
+                    html2canvas(srcElement, {backgroundColor: null}).then((canvas) => {
                         if (btn.id === "saveImageButton") {
-                            console.log("SAVING");
                             const newTab = window.open();
                             const newDoc = newTab.document;
-                            newDoc.write('<html><body></body></html>'); // Create a new document
+                            newDoc.write('<html><body><h1 style="text-align:center;">Right Click To Save Image</h1></body></html>'); // Create a new document
                             newDoc.body.appendChild(canvas); // Append the canvas to the body of the new document
                             newDoc.close(); // Close the document after adding the canvas
                         }
                     });
+                    srcElement.style.backgroundColor = "white";
                 }
                 else {
                     const srcElement = document.querySelector("#" + divId[i]);
-                    html2canvas(srcElement).then((canvas) => {
+                    html2canvas(srcElement, {backgroundColor: null}).then((canvas) => {
                         if (btn.id === "saveImageButton") {
                             const newTab = window.open();
                             const newDoc = newTab.document;
-                            newDoc.write('<html><body></body></html>'); // Create a new document
+                            newDoc.write('<html><body><h1 style="text-align:center;">Right Click To Save Image</h1></body></html>'); // Create a new document
                             newDoc.body.appendChild(canvas); // Append the canvas to the body of the new document
                             newDoc.close(); // Close the document after adding the canvas
                         }
